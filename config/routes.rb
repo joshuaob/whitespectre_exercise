ee03b10e7c409c9910d79d3cc70f38f032c55e18
Joshua Ob <joshua.ob@outlook.com>
2016-11-08 12:32:34 +0000

    add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    resources :group_events, only: [:index, :show, :update, :destroy, :create] do
+      post 'publish', on: :member
+    end
+  end
+
 end
